fb286e68612531f770824baded7e7b7ec8db672e
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2021-07-10 09:24:18 +0530

    Update server.ts
    Updatd

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import addonInit from './addon';
 import { serveHTTP } from 'stremio-addon-sdk';
 
-addonInit().then(addonInterface => serveHTTP(addonInterface, { port:  +process.argv[2] || 7000 }));
+addonInit().then(addonInterface => serveHTTP(addonInterface, { port:  +process.argv[2] || 8080 }));
 
 
